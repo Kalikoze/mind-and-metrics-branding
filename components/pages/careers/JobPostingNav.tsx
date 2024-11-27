956398b93a879c1cba517415736ecc2d21dfd4e0
Travis Rollins <kalikoze@gmail.com>
2024-11-27 12:32:47 -0600

    Refactor: Break job posting into more modular components and make HTML much more semantic

---
+++ components/pages/careers/JobPostingNav.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+import { HiOutlineArrowLeft } from 'react-icons/hi2';
+import ScrambleText from '@/components/common/ScrambleText';
+
+interface JobPostingNavProps {
+  hoveringBack: boolean;
+  setHoveringBack: (hovering: boolean) => void;
+}
+
+const JobPostingNav = ({ hoveringBack, setHoveringBack }: JobPostingNavProps) => (
+  <nav>
+    <Link
+      href="/careers"
+      data-cy="back-link"
+      onMouseEnter={() => setHoveringBack(true)}
+      onMouseLeave={() => setHoveringBack(false)}
+      className="inline-flex items-center gap-2 text-secondary-400 hover:text-secondary-500 
+                 transition-colors duration-300 mb-6 group"
+    >
+      <HiOutlineArrowLeft className="w-5 h-5 transition-transform duration-300 
+                                 group-hover:-translate-x-1" />
+      <span className="w-[160px]">
+        <ScrambleText
+          text="Back to Careers"
+          isHovering={hoveringBack}
+        />
+      </span>
+    </Link>
+  </nav>
+);
+
+export default JobPostingNav; 
